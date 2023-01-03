8e3c94021ca0051617e94bb484f623cfc1be51e4
alixcaroline <alixcaroline13@gmail.com>
2023-01-03 13:15:44 +0100

    hash password before saving user

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const UserSchema = mongoose.Schema({
 	name: {
@@ -16,6 +17,14 @@ const UserSchema = mongoose.Schema({
 		type: String,
 		required: true,
 	},
+});
+
+//hash password before saving user
+UserSchema.pre('save', async function (next) {
+	if (this.isModified('password')) {
+		this.password = await bcrypt.hash(this.password, 10);
+	}
+	next();
 });
 
 module.exports = mongoose.model('User', UserSchema);
