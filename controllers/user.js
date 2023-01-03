58916be8c7940b3090c0dd9b5b307a3a69186568
alixcaroline <alixcaroline13@gmail.com>
2023-01-03 13:22:28 +0100

    check for duplicate email

--- controllers/user.js
+++ controllers/user.js
@@ -3,9 +3,14 @@ const User = require('../models/user');
 exports.create = async (req, res) => {
 	const { name, email, password } = req.body;
 
+	//check if email exists already
+	const duplicate = await User.findOne({ email });
+	if (duplicate) {
+		return res.status(401).json({ error: 'This email is already in use' });
+	}
 	const newUser = new User({ name, email, password });
 
 	await newUser.save();
 
-	res.json({ user: newUser });
+	res.status(201).json({ user: newUser });
 };
