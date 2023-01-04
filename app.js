60ff3620116af87c5901ce35f92ff81f6128914f
alixcaroline <alixcaroline13@gmail.com>
2023-01-04 12:26:35 +0100

    add error handling

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+require('express-async-errors');
+const { errorHandler } = require('./middlewares/errorHandler');
 
 //routes
 const userRouter = require('./routes/user');
@@ -9,6 +11,8 @@ app.use(express.json());
 require('./db/');
 
 app.use('/api/user', userRouter);
+
+app.use(errorHandler);
 
 app.listen(process.env.PORT, () =>
 	console.log('server app listening on port ' + process.env.PORT),
