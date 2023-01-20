0cbd07a00f4cfe428882c91aaa712de6bad71f42
alixcaroline <alixcaroline13@gmail.com>
2023-01-20 19:39:34 +0100

    authentication token

--- controllers/user.js
+++ controllers/user.js
@@ -221,7 +221,7 @@ exports.signIn = async (req, res) => {
 
 	const { _id, name } = user;
 
-	const jwtToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+	const jwtToken = jwt.sign({ userId: _id }, process.env.JWT_SECRET);
 
-	res.json({ user: { id: user._id, name, email, token: jwtToken } });
+	res.json({ user: { id: _id, name, email, token: jwtToken } });
 };
